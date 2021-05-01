b52512c98d5894539de27debd18f2b739e7aa8d2
Dakshay <dakshayahuja02@gmail.com>
2021-05-01 20:23:51 +0530

    carousel done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel();
-  });
-
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  stagePadding: 60,
+  margin: 20,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  animateOut: 'fadeOut',
+  dots: false,
+  nav: true,
+  navText: ["<div class='nav-button owl-prev'>‹</div>", "<div class='nav-button owl-next'>›</div>"],
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 4
+    }
+  }
+})
