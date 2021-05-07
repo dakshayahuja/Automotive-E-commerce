fd5452027b47cace92726396367c84a8a8027c58
Dakshay <dakshayahuja02@gmail.com>
2021-05-07 22:03:58 +0530

    Completed the products pages and about us section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $('.owl-carousel').owlCarousel({
   loop: true,
-  stagePadding:50,
-  center:true,
-  margin: 20,
+  // stagePadding: 50,
+  center: true,
+  margin: 30,
   autoplay: true,
   autoplayTimeout: 3000,
   autoplayHoverPause: true,
@@ -12,29 +12,29 @@ $('.owl-carousel').owlCarousel({
   // navText: ["<div class='nav-button owl-prev'>‹</div>", "<div class='nav-button owl-next'>›</div>"],
   responsive: {
     0: {
-      items: 1
-    },
-    600: {
       items: 2
     },
-    1000: {
+    600: {
       items: 3
+    },
+    1000: {
+      items: 5
     }
   }
 });
 
-$(document).on('click','#btn1',function() {
+$(document).on('click', '#btn1', function () {
   window.open("./pages/products.html", "_self");
 });
 
-$(document).on('click','#next-btn',function() {
+$(document).on('click', '#next-btn', function () {
   window.open("./products2.html", "_self");
-});  
+});
 
-$(document).on('click','#prev-btn',function() {
+$(document).on('click', '#prev-btn', function () {
   window.open("./products.html", "_self");
-});  
+});
 
-$(document).on('click','.navbar-brand',function() {
+$(document).on('click', '.navbar-brand', function () {
   window.open("../index.html", "_self");
 });
