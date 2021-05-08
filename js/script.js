b5e94ba9dcad762aef423f11a70957555a048986
Dakshay <dakshayahuja02@gmail.com>
2021-05-08 21:27:42 +0530

    implemented modal on contact us -give requirements

--- js/script.js
+++ js/script.js
@@ -39,3 +39,23 @@ $(document).on('click', '.navbar-brand', function () {
   window.open("../index.html", "_self");
 });
 
+    let modalBtns = [...document.querySelectorAll("#quote")];
+      modalBtns.forEach(function(btn) {
+        btn.onclick = function() {
+          let modal = btn.getAttribute('data-modal');
+          document.getElementById(modal)
+            .style.display = "block";
+        }
+      });
+     let closeBtns = [...document.querySelectorAll(".close")];
+      closeBtns.forEach(function(btn) {
+        btn.onclick = function() {
+          let modal = btn.closest('.modal');
+          modal.style.display = "none";
+        }
+      });
+     window.onclick = function(event) {
+        if(event.target.className === "modal") {
+          event.target.style.display = "none";
+        }
+      }
